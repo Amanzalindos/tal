c4cd4a948f897059b50d7d545cfbadf580c4e6f2
Amanzalindos <miguel.paezp@uniagustiniana.edu.co>
2023-09-04 19:27:19 -0500

    first commit

---
+++ app/src/main/java/com/example/oper/operaciones.java
@@ -0,0 +1,30 @@
+package com.example.oper;
+
+public class operaciones {
+
+        public String suma(String numero1, String numero2) {
+            int resultado = Integer.valueOf(numero1) + Integer.valueOf(numero2);
+            return String.valueOf(resultado);
+        }
+
+        public String resta(String numero1, String numero2) {
+            int resultado = Integer.valueOf(numero1) - Integer.valueOf(numero2);
+            return String.valueOf(resultado);
+        }
+
+        public String multiplicar(String numero1, String numero2) {
+            int resultado = Integer.valueOf(numero1) * Integer.valueOf(numero2);
+            return String.valueOf(resultado);
+        }
+
+        public String dividir(String numero1, String numero2) {
+            int resultado = Integer.valueOf(numero1) / Integer.valueOf(numero2);
+            return String.valueOf(resultado);
+        }
+
+        public String Alcuadrado(String numero1, String numero2) {
+            int resultado = Integer.valueOf(numero1) / Integer.valueOf(numero2) * 2;
+            return String.valueOf(resultado);
+        }
+    }
+
